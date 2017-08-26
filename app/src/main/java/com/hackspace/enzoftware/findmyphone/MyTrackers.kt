a03ae40933232ad7018f69c76c59869fa6180b4a
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-08-26 04:27:36 -0500

    Dummy data

--- app/src/main/java/com/hackspace/enzoftware/findmyphone/MyTrackers.kt
+++ app/src/main/java/com/hackspace/enzoftware/findmyphone/MyTrackers.kt
@@ -1,19 +1,34 @@
 package com.hackspace.enzoftware.findmyphone
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_my_trackers.*
+import kotlinx.android.synthetic.main.contact_ticket.view.*
 
 class MyTrackers : AppCompatActivity() {
-
+    var adapterTicket:ContactAdapter ?= null
+    var contactsList = ArrayList<UserContact>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_trackers)
+        dummyData()
+        adapterTicket = ContactAdapter(this,contactsList)
+        lvContacts.adapter = adapterTicket
+    }
+
+
+    // Just for debug
+    fun dummyData(){
+        contactsList.add(UserContact("Enzoftware","994050397"))
+        contactsList.add(UserContact("RodrigoGmi2","123456789"))
+        contactsList.add(UserContact("Daztery","987456321"))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        var inflater = menuInflater
+        val inflater = menuInflater
         inflater.inflate(R.menu.tracker_menu,menu)
         return true
     }
@@ -33,5 +48,39 @@ class MyTrackers : AppCompatActivity() {
             }
         }
         return true
+    }
+
+
+    class ContactAdapter:BaseAdapter{
+        var context:Context ?= null
+        var contactsList = ArrayList<UserContact>()
+
+        constructor(context: Context,contactList: ArrayList<UserContact>){
+            this.context = context
+            this.contactsList = contactList
+        }
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val userContact = contactsList[p0]
+            val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val contactTicketView = inflater.inflate(R.layout.contact_ticket,null)
+            contactTicketView.tvName.text = userContact.name
+            contactTicketView.tvPhone.text = userContact.phone
+
+            return contactTicketView
+
+        }
+
+        override fun getItem(p0: Int): Any {
+            return contactsList[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+            return contactsList.size
+        }
+
     }
 }
---
+++ app/src/main/java/com/hackspace/enzoftware/findmyphone/UserContact.kt
@@ -0,0 +1,14 @@
+package com.hackspace.enzoftware.findmyphone
+
+/**
+ * Created by enzoftware on 8/26/17.
+ */
+
+class UserContact{
+    var name:String ?= null
+    var phone:String ?= null
+    constructor(name: String , phone:String){
+        this.name = name
+        this.phone = phone
+    }
+}
