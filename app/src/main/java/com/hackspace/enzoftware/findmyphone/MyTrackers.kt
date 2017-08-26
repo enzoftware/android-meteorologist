1dba19acb21756fe7e52385123d9d898f2b9ad20
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-08-25 22:46:29 -0500

    new menu & add trackers

--- app/src/main/java/com/hackspace/enzoftware/findmyphone/MyTrackers.kt
+++ app/src/main/java/com/hackspace/enzoftware/findmyphone/MyTrackers.kt
@@ -1,38 +1,31 @@
 package com.hackspace.enzoftware.findmyphone
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity() {
+class MyTrackers : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val userData = UserData(this)
-        userData.loadPhoneNumber()
+        setContentView(R.layout.activity_my_trackers)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         var inflater = menuInflater
-        inflater.inflate(R.menu.main_menu,menu)
+        inflater.inflate(R.menu.tracker_menu,menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item!!.itemId){
-            R.id.AddItem -> {
-                val intent = Intent(this,MyTrackers::class.java)
-                startActivity(intent)
+            R.id.addContat -> {
+                //TODO : Add new contact
             }
 
-            R.id.HelpItem -> {
-                //TODO : Implement an activity
+            R.id.finishActivity -> {
+                finish()
             }
 
             else->{
