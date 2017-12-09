c7085c9ac67eeaee35e685d0f445b8f6eeec23fb
enzoftware <lizama.enzo@gmail.com>
2017-12-09 02:47:30 -0500

    Finish him

--- server.js
+++ server.js
@@ -40,6 +40,37 @@ server.route({
     }
 });
 
+
+server.route({
+    method: 'GET',
+    path: '/teams/{id}',
+    handler: function (request, reply) {
+        const teamID = encodeURIComponent(request.params.id);
+        console.log(teamID);
+        
+        Request.get(`http://api.football-data.org/v1/teams/${teamID}`, function (error, response, body) {
+            if (error) {
+                throw error;
+            }
+ 
+            const result = JSON.parse(body);
+            console.log(result);
+ 
+            Request.get(`http://api.football-data.org/v1/teams/${teamID}/fixtures`, function (error, response, body) {
+                if (error) {
+                    throw error;
+                }
+ 
+                const fixtures = LodashTake(LodashFilter(JSON.parse(body).fixtures, function (match) {
+                    return match.status === 'SCHEDULED';
+                }), 5);
+ 
+                reply.view('team', { result: result, fixtures: fixtures });
+            });
+        });
+    }
+});
+
 Handlebars.registerHelper('teamID',function (teamURL){
     return teamURL.slice(38);
 });
